5e68c5e86f6ac2d26b84be3730d653445505f264
yangxufeng <yangxufeng@yingroup.com>
2019-03-14 10:32:45 +0800

    feat(module): 1.补全已有命令文件

---
+++ src/command/list.js
@@ -0,0 +1,10 @@
+const COMMAND = require('../utils/command');
+const MESSAGE = require('../utils/message');
+const ACCOUNT = require('../core/account.core');
+
+exports.run = function (arg) {
+  return new Promise((resolve, reject) => {
+    MESSAGE.ShowModel(ACCOUNT.GetUsre());
+    resolve();
+  })
+}
